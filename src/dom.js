470b56e7d70ed2ea29c9dbfd8f0d4b12ea4196b6
Zhong Xuan <hewzhongxuan6f@gmail.com>
2022-12-08 22:26:17 +0800

    Finished up with the html skeleton, created buttons, project modal
    Simple javascript handlers and DOM event changers

---
+++ src/dom.js
@@ -0,0 +1,65 @@
+const dom = (()=>{
+    const body = document.querySelector('body')
+    const addProjectModal = document.querySelector('#add-proj-modal')
+
+    function showModal(modal){
+        modal.classList.remove('hide')
+        modal.classList.add('display')
+    }
+
+    function hideModal(modal){
+        modal.classList.remove('display')
+        modal.classList.add('hide')
+    }
+
+    function activeLink(link){
+        const navLink = document.querySelector('.sidebar-link')
+        navLink.forEach((element)=>{
+            element.classList.remove('active')
+        })
+        if(link.classList.contains('fa')){
+            link.parentElement.classList.add('active')
+        }
+        else{
+            link.classList.add('active')
+        }
+    }
+
+    function activeProject(project){
+        const projectLink = document.querySelectorAll('.sidebar-project');
+        projectLink.forEach((element)=>{
+            element.classList.remove('active')
+        })
+        if(project.classList.contains('fa-edit')){
+            project.parentElement.classList.add('active')
+        }
+        else{
+            project.classList.add('active')
+        }
+    }
+
+    function activeProjectIcon(icon){
+        const projectIcon = document.querySelectorAll('.project-icon')
+        projectIcon.forEach((element)=>{
+            element.classList.remove('active')
+        })
+        if(icon.classList.contains('fa')){
+            icon.parentElement.classList.add('active')
+        }
+        else{
+            icon.classList.add('active')
+        }
+    }
+
+    return{
+        body,
+        addProjectModal,
+        showModal,
+        hideModal,
+        activeLink,
+        activeProject,
+        activeProjectIcon
+    }
+})();
+
+export default dom;
---
+++ src/handler.js
@@ -0,0 +1,30 @@
+import { ESLint } from "eslint";
+import dom from "./dom";
+
+const handler = (()=>{
+    function clickHandler(){
+        dom.body.addEventListener('click',(e)=>{
+            //Nav
+            if(e.target.classList.contains('sidebar-link')){
+                console.log('nav link')
+                dom.activeLink(e.target)
+            }
+            //Sidebar Project
+            else if(e.target.classList.contains('sidebar-project')){
+                console('sidebar project')
+                dom.activeProject(e.target)
+            }
+            //Open add project modal
+            else if(e.target.classList.contains('add-proj-modal')){
+                dom.showModal(e.target)
+                console.log('open proj modal')
+            }
+            //Add project icon
+            else if(e.target.classList.contains('project-icon')){
+                dom.activeProjectIcon(e.target)
+                console.log('Select project icon')
+            }
+            //Continue tomorrow
+        })
+    }
+})
